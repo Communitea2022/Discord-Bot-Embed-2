00acb712e793c4eb70053c2d22b8205fd932f4f6
UpSide - Developing <98348216+UpSideDeveloping@users.noreply.github.com>
2022-01-27 16:33:16 +0100

    Embed komanda

---
+++ main.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+
+#prefix za bota
+client = commands.Bot(command_prefix=">")
+
+#Pokretanje Bota
+@client.event
+async def on_ready():
+    print("[+] Bot je pokrenut.")
+
+@client.command()
+async def test(ctx):
+    embed = discord.Embed(
+        colour = discord.Colour.purple()
+    )
+
+    embed.add_field(name = ":fire: UPSIDE" , value = "***UPSIDE - DEVELOPING***")
+    await ctx.send(embed = embed)
+
+#Komanda za Bota
+@client.command()
+async def hello(ctx):
+    await ctx.send("Zdravo")
+
+client.run("OTM2MjcxMTg0Nzg0NDAwNDk1.YfKwfQ.SXV1IAa3cZhEqq95347CJo3tyE0")
